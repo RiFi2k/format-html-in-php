0737feeaf34f17c2ec46a5f497ae4e25b3681044
Reilly Lowery <reilly@reillylowery.io>
2018-08-10 19:47:31 -0400

    Add document selector to language identifier
    Get rid of the vscode warning

--- src/extension.ts
+++ src/extension.ts
@@ -4,20 +4,26 @@ import * as vscode from "vscode";
 import formatHtml from "./formatHtml";
 
 export function activate(context: vscode.ExtensionContext) {
-  vscode.languages.registerDocumentFormattingEditProvider("php", {
-    provideDocumentFormattingEdits(
-      document: vscode.TextDocument
-    ): vscode.TextEdit[] {
-      const originalText = document.getText();
-      const lastLine = document.lineAt(document.lineCount - 1);
-      const range = new vscode.Range(new vscode.Position(0, 0), lastLine.range.end);
-      let options = {};
-      const htmlOptions = Object.assign(
-        options,
-        vscode.workspace.getConfiguration("html").get("format")
-      );
-      const modifiedText = formatHtml(originalText, htmlOptions);
-      return [vscode.TextEdit.replace(range, modifiedText)];
+  vscode.languages.registerDocumentFormattingEditProvider(
+    { scheme: "file", language: "php" },
+    {
+      provideDocumentFormattingEdits(
+        document: vscode.TextDocument
+      ): vscode.TextEdit[] {
+        const originalText = document.getText();
+        const lastLine = document.lineAt(document.lineCount - 1);
+        const range = new vscode.Range(
+          new vscode.Position(0, 0),
+          lastLine.range.end
+        );
+        let options = {};
+        const htmlOptions = Object.assign(
+          options,
+          vscode.workspace.getConfiguration("html").get("format")
+        );
+        const modifiedText = formatHtml(originalText, htmlOptions);
+        return [vscode.TextEdit.replace(range, modifiedText)];
+      }
     }
-  });
+  );
 }
