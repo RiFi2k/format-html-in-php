e263623d09f75790abfece46caec7bfc99c89c80
RiFi2k <reilly@rifi.me>
2019-01-03 16:44:01 -0500

    PHP scoped format on save refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 import * as get from 'lodash.get';
+import * as has from 'lodash.has';
 import * as vscode from 'vscode';
 import beautifyHtml from './beautifyHtml';
 import optionsFromVSCode from './optionsFromVsCode';
@@ -14,11 +15,18 @@ class DocumentWatcher {
     subscriptions.push(vscode.workspace.onWillSaveTextDocument(async e => {
       const activeDoc = this.getActiveDoc(vscode.window.activeTextEditor);
       if (activeDoc.languageId === 'php') {
+        const editor = vscode.window.activeTextEditor;
+        const position = editor.selection.active;
+        const newPosition = position.with(position.line, 0);
+        console.log(position,newPosition);
+        const newSelection = new vscode.Selection(newPosition, newPosition);
         const transformations = this.doPreSaveTransformations(
           e.document,
           e.reason
         );
         e.waitUntil(transformations);
+        await transformations;
+        editor.selection = newSelection;
       }
     }));
     this.disposable = vscode.Disposable.from.apply(this, subscriptions);
@@ -37,8 +45,15 @@ class DocumentWatcher {
     reason: vscode.TextDocumentSaveReason
 	): Promise<vscode.TextEdit[]> {
     const config = vscode.workspace.getConfiguration();
-    const phpConfig = vscode.workspace.getConfiguration().get('[php]')['editor.formatOnSave'];
-    if (config.editor.formatOnSave === true || phpConfig === true) {
+    const phpScopedFormat = has(config, '[php]');
+    let phpScopedFormatVal = false;
+    if (phpScopedFormat === true) {
+      const phpScopedObj = get(config, '[php]');
+      if (phpScopedObj['editor.formatOnSave'] === true) {
+        phpScopedFormatVal = true;
+      }
+    }
+    if (config.editor.formatOnSave === true || phpScopedFormatVal === true) {
       const activeDoc = this.getActiveDoc(vscode.window.activeTextEditor);
       const lastLine = activeDoc.lineAt(activeDoc.lineCount - 1);
       const range = new vscode.Range(
