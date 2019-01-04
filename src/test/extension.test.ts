48165e4c87f6c795cd7cdf133d9ff5a8534242d7
RiFi2k <reilly@rifi.me>
2019-01-03 19:56:29 -0500

    Remove test folder

--- src/test/extension.test.ts
+++
@@ -1,9 +0,0 @@
-//import * as assert from 'assert';
-import * as vscode from 'vscode';
-//import * as formatPhpInHtml from '../extension';
-//var expect = require('chai').expect;
-//var should = require('chai').should();
-
-test("Something 1", function() {
-    console.log(vscode)
-});
